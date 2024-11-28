4c30cfe7f4affbcbc80a9482faacdc9d2457a5ca
Louie-lasse <love.lindqvist101@gmail.com>
2024-11-28 15:03:30 +0100

    Minor db fixes

--- commands/list_users.py
+++ commands/list_users.py
@@ -7,12 +7,12 @@ class List_Users(Command):
         super().__init__()
         self.db = DatabaseHandler()
 
-    def execute(self, _, arg: str, say):
-        if len(arg) > 0:
-            if not arg.isnumeric() or int(arg) != float(arg):
+    def execute(self, _, args: str, say):
+        if len(args) > 0:
+            if not args.isnumeric() or int(args) != float(args):
                 say("Please provide an integer value, or nothing to get the default `10`")
                 return
-            n = int(arg)
+            n = int(args)
         else:
             n = 10
         users = self.db.list_users(n)
