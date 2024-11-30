88c794110899bbb213934b779878126b737827e5
Louie-lasse <love.lindqvist101@gmail.com>
2024-11-30 23:20:31 +0100

    Bug

--- commands/whoami.py
+++ commands/whoami.py
@@ -4,7 +4,7 @@ from . import Command
 class Whoami(Command):
 
     def execute(self, user_ids, args: str, say):
-        say(f"{user_ids["slack_id"]}")
+        say(f"{user_ids['slack_id']}")
     
     def help(self):
         return "Skriver ut dit slack_id. Änvänds bara vid byte av IT-ansvarig"
