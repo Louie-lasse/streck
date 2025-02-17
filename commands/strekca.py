34fcda38bc825a0b594c32207dbdacef33c071e3
Louie-lasse <love.lindqvist101@gmail.com>
2025-02-17 19:13:36 +0100

    Adding multi-streck

--- commands/strekca.py
+++ commands/strekca.py
@@ -1,5 +1,9 @@
+from abc import abstractmethod
+import re
 from db_handler import DatabaseHandler
 from . import Command
+
+A_LOT = 12
 
 class Strecka(Command):
     """
@@ -12,14 +16,36 @@ class Strecka(Command):
         self.db = DatabaseHandler()
 
     def execute(self, user_ids, args : str, say):
+
+        pattern = r'^(\d+)?(!)?$'
+        match = re.match(pattern, args)
+        if not match:
+            say("\n".join([ f"Fattar inte helt `{args}`. Kör",
+                           f"`{self.usage()}` eller bara {self.__cmd__()} för att strecka"
+                           ]))
+            return
+        
+        amount = int(match.group(1)) if match.group(1) else 1
+        important = bool(match.group(2))
+
+        if amount < 1:
+            say(f"Vafan menar du? Du kan ju inte sträcka {amount}")
+
+        if amount > A_LOT and not important:
+            say(f"Är du säker på att du vill sträcka {amount} st?! Kör `{self.__cmd__()} {amount}!` för att bekräfta")
+
         price = self.db.get_price(self.product)
         if price <= 0:
             say(f"Hmmm. Något gick fel. Kontakta <@{self._ADMIN}> om saker inte verkar funka")
             return
-        
-        changes = self.db.purchase(user_ids["db_id"], self.product, price)
-        
-        if not changes:
-            say(f"Hmmm. Något gick fel. Kontakta <@{self._ADMIN}> om saker inte verkar funka")
-            return
-        say("Har streckat :crown:")
+        for i in range(amount):
+            changes = self.db.purchase(user_ids["db_id"], self.product, price)
+            
+            if not changes:
+                say(f"Hmmm. Något gick fel. Kontakta <@{self._ADMIN}> om saker inte verkar funka"
+                    + (f"\nSträckade {i} gånger iallafall" if i else ""))
+                return
+        say("Har streckat " + (f"{amount} st" if amount > 1 else "") + " :crown:")
+
+    def usage(self):
+        return f"{self.__cmd__()} [<antal>]"
