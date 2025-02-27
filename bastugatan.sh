f2d0a3d0c96d7b76be38566a18beeeebd1576767
Louie-lasse <love.lindqvist101@gmail.com>
2025-02-27 14:11:00 +0100

    Echo changes

--- bastugatan.sh
+++ bastugatan.sh
@@ -5,12 +5,15 @@
 cd /path/to/your/streck || { echo "Failed to change directory"; exit 1; }
 
 # Ensure that the network connection has been established
-sleep 2
+echo "Waiting for network connection..."
+sleep 15
 
 # Launch the Python script in a terminal window.
+echo "Starting scripts"
 lxterminal -e "bash -c 'python3 /path/to/startup_script.py; exec bash'" &
 
 # Wait a few seconds to ensure the Python script is up and the network is ready
+echo "Waiting for the script to start..."
 sleep 5
 
-chromium-browser --start-fullscreen --force-device-scale-factor=0.9 http://localhost:5000 &
+chromium-browser --start-fullscreen --force-device-scale-factor=0.9 http://localhost:5000
