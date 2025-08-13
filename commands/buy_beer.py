d3963d969d556e4ce5bc949763c7bfd96601d715
Louie-lasse <love.lindqvist101@gmail.com>
2025-08-13 12:06:37 +0200

    Adding sprit

--- commands/buy_beer.py
+++ commands/buy_beer.py
@@ -1,5 +1,6 @@
 from .strecka import Strecka
 
+
 class Beer(Strecka):
 
     def __init__(self):
@@ -7,9 +8,9 @@ class Beer(Strecka):
 
     def help(self):
         return "DU! Om du inte klarar av att sträcka själv får du fan inget"
-    
+
     def description(self):
         return "HUhmbrbrbrbbrbr... BÄRS!!!"
-    
+
     def __cmd__(self):
         return "öl"
---
+++ commands/fin_sprit.py
@@ -0,0 +1,20 @@
+from .strecka import Strecka
+
+class FinSprit(Strecka):
+    def __init__(self):
+        super().__init__(0, 0.5)
+        # todo replace `0` with the actual product ID for FinSprit
+
+    def help(self):
+        return "Antal cl finsprit du vill sträcka. "\
+                "Notera att vanliga sträckkoden är för 2 cl. "\
+                f"Skriv e.g. `{self.__cmd__()} 12` för en 12:a"
+
+    def description(self):
+        return "Sträcka finsprit (cl)"
+
+    def __cmd__(self):
+        return "fin"
+
+    def usage(self):
+        return f"{self.__cmd__()} [<antal cl>]"
