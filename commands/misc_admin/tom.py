eb70bc11e6a4b5aa12d3d4166238e8d40a121a36
Louie-lasse <love.lindqvist101@gmail.com>
2026-04-03 20:08:15 +0200

    empty by dbid

--- commands/misc_admin/tom.py
+++ commands/misc_admin/tom.py
@@ -15,7 +15,7 @@ class Tom(Command):
         self.db = DatabaseHandler()
 
     def _get_users(self, args: str, say):
-        user_ids = re.findall(r"<@([A-Z0-9]+)>", args)
+        user_ids = re.findall(r"(?:<@)?(\d+|[A-Z0-9]+)(?:>)?", args)
         if not user_ids:
             say("Ingen användare angiven")
             say(f"Användning: {self._usage()}")
@@ -26,8 +26,13 @@ class Tom(Command):
             db_user = self.db.get_user(uid)
             if db_user and db_user[0] is not None:
                 res.append((db_user[0], uid))
-            else:
-                say(f"<@{uid}> är inte kopplad till något konto")
+                continue
+            if all([c.isdigit() for c in uid]):
+                slack_id = self.db.get_slack_id(uid)
+                if slack_id:
+                    res.append((int(uid), slack_id))
+                    continue
+            say(f"<@{uid}> är inte kopplad till något konto")
 
         if not res:
             say("Inga giltiga användare angivna")
@@ -44,7 +49,7 @@ class Tom(Command):
             say(f"<@{slack_id}> har ingen skuld")
             return
         if debt <= 0:
-            say(f"<@{slack_id}> du verkar vara skyldig <@{slack_id}> pengar. \nDu ska swisha dom {debt}.")
+            say(f"Du var skyldig <@{user_id}> pengar. \nDu ska swisha dom {debt}.")
         changes = self.db.purchase(user_id, None, -debt, paid=True)
         if changes <= 0:
             say(f"Något gick fel när skulden för <@{slack_id}> skulle nollställas")
@@ -56,9 +61,6 @@ class Tom(Command):
 
 
     def execute(self, _, args: str, say):
-        if not args:
-            say(self._usage())
-            return
         user_ids = self._get_users(args, say)
         if not user_ids:
             return
@@ -66,12 +68,13 @@ class Tom(Command):
             self.clear_debt(user[0], user[1], say)
 
     def _usage(self):
-        return f"`{self.__cmd__()} @user`"
+        return f"`{self.__cmd__()} [@user | databasid]`"
 
     def help(self):
         return "Används för att nollställa en persons skuld\n"\
                 f"Använding: {self._usage()}\n"\
-                    "Kan även göras på flera personer samtidigt"
+                    "Kan även göras på flera personer samtidigt"\
+                    f"e.g. `{self.__cmd__()} @user1 @user2 databasid1`"
     
     def description(self):
         return "Nollställ en persons skuld"
