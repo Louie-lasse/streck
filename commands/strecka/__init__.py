41de42422a4e384bb8114e7e5af2512948e5ebf5
Felix <felix.vitmossen@gmail.com>
2026-05-05 18:59:25 +0200

    fixad min förra commit

--- commands/strecka/__init__.py
+++ commands/strecka/__init__.py
@@ -3,4 +3,5 @@ from .buy_cider import Cider
 from .buy_soda import Soda
 from .buy_fin_sprit import FinSprit
 from .buy_bib import Bib
+from .change_price import ChangePrice
 from .prislista import Prislista
