b5865df3dc766e0b9f08393521bd61e278b63932
Louie-lasse <love.lindqvist101@gmail.com>
2026-05-06 17:00:06 +0200

    oops

--- db_handler.py
+++ db_handler.py
@@ -206,6 +206,6 @@ class DatabaseHandler:
         updates the price of a product
         """
         q = "UPDATE products SET price=? WHERE id = ?"
-        res = self.execute_command(q, (product, price))
+        res = self.execute_command(q, (price, product))
 
         return 0 if res <= 0 else res
